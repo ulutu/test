be398945df6784f274dcd7038dffa27306a9cdcb
Chris Rosebrugh <crosebrugh@gmail.com>
2014-02-02 11:06:18 -0800

    minimal app

--- app/lib/yapper/log.rb
+++
@@ -1,5 +0,0 @@
-if defined?(Motion::Yapper::Log)
-  Yapper::Yapper::Log = Motion::Yapper::Log
-  Yapper::Log.addYapper::Logger DDTTYYapper::Logger.sharedInstance
-  Yapper::Log.addYapper::Logger DDASLYapper::Logger.sharedInstance
-end
--- app/lib/yapper/version.rb
+++
@@ -1,3 +0,0 @@
-module Yapper
-  VERSION = '0.0.1'
-end
